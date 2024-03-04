Fixed metric calculation bug, added better metric handling for value and token predictions.

Long-term_Forecasting/main.py
@@ -63,7 +63,12 @@ print(f"Using config {args.model_config}")
 # Get Data
 if args.freq == 0:
     args.freq = 'h'
-
+train_data, train_loader = data_provider(args, 'train')
+vali_data, vali_loader = data_provider(args, 'val')
+test_data, test_loader = data_provider(args, 'test')
+if args.freq != 'h':
+    args.freq = utils.SEASONALITY_MAP[test_data.freq]
+    print("freq = {}".format(args.freq))
 
 
 #########################
@@ -71,12 +76,6 @@ if args.freq == 0:
 #########################
 
 if args.train or args.pretrain_embeddings:
-    train_data, train_loader = data_provider(args, 'train')
-    vali_data, vali_loader = data_provider(args, 'val')
-    test_data, test_loader = data_provider(args, 'test')
-    if args.freq != 'h':
-        args.freq = utils.SEASONALITY_MAP[test_data.freq]
-        print("freq = {}".format(args.freq))
     
     print("TRAINING")
     # Get Configuration
@@ -194,8 +193,8 @@ if args.train or args.pretrain_embeddings:
         #for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in tqdm(enumerate(train_loader), disable=args.is_slurm):
         for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
 
-            #if i > 10:
-            #    break
+            if i > 3:
+                break
             #T0 = time.time()
             iter_count += 1
             model_optim.zero_grad()
@@ -257,7 +256,7 @@ if args.train or args.pretrain_embeddings:
             state=utils.CheckpointState(model, model_optim, None),
             epochs=epoch+1,
             train_steps=epoch,
-            min_valid_loss=min_valid_loss,
+            min_valid_loss=float(min_valid_loss),
             #metrics=ema_train_metrics.get_metrics(),
             is_optimal=False,
         )
@@ -275,7 +274,6 @@ if args.train or args.pretrain_embeddings:
             is_training=True,
             pretrain_embeddings=args.pretrain_embeddings
         )
-        print(valid_metrics)
         #valid_loss = vali(model, vali_data, vali_loader, criterion, args, device, ii)
         # test_loss = vali(model, test_data, test_loader, criterion, args, device, ii)
         # print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f}, Test Loss: {4:.7f}".format(
@@ -283,6 +281,17 @@ if args.train or args.pretrain_embeddings:
         print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f}".format(
             epoch + 1, train_steps, train_loss, valid_metrics['loss']))
         if is_wandb:
+            log_metrics = {
+                k : v for k, v in valid_metrics.items()\
+                    if 'std' not in k and\
+                    ('loss' in k or 'mse' in k or 'mae' in k or 'mse' in k\
+                     or 'cross_entropy' in k or 'accuracy' in k) 
+            }
+            log_metrics['train_loss'] = train_loss
+            log_metrics['valid_loss'] = valid_metrics['loss']
+            log_metrics['epoch'] = epoch + 1
+            del log_metrics['loss']
+            """
             log_metrics = {
                 'train_loss' : train_loss,
                 'valid_loss' : valid_metrics['loss'],
@@ -292,6 +301,7 @@ if args.train or args.pretrain_embeddings:
                 'token_token' : valid_metrics['token_token'],
                 'epoch' : epoch+1,    
             }
+            """
             wandb.log(log_metrics, step=epoch+1)
         
         if valid_metrics['loss'] < min_valid_loss:
@@ -300,7 +310,7 @@ if args.train or args.pretrain_embeddings:
                 state=utils.CheckpointState(model, model_optim, None),
                 epochs=epoch+1,
                 train_steps=epoch,
-                min_valid_loss=min_valid_loss,
+                min_valid_loss=float(min_valid_loss),
                 is_optimal=True
             )
 
@@ -318,7 +328,7 @@ if args.train or args.pretrain_embeddings:
 #####  Evaluate  #####
 ######################
 print("Evaluating Test Set")
-args.label_len = 0
+#args.label_len = 0
 #train_data, train_loader = data_provider(args, 'train')
 vali_data, vali_loader = data_provider(args, 'val')
 """
@@ -437,7 +447,9 @@ for ii in itr_range:
         curves.append(copy(itr_results))
 
         if not args.noise_curve:
-            metric_dict = utils.metrics_to_dict(curves[-1])
+            metric_dict = utils.MetricCalculator.metrics_to_dict(
+                curves[-1], type='values'
+            )
             utils.save_test_results(metric_dict, checkpoint_model_dir)
 
         #best_model_path = path + '/' + 'checkpoint.pth'
@@ -450,8 +462,12 @@ for ii in itr_range:
         sys.exit(0)
     curves = np.array(curves)
     if args.noise_curve:
-        metric_dict = utils.metrics_to_dict(curves.transpose())
-        utils.save_noise_results(metric_dict, noise_scales, checkpoint_model_dir, noise_label)
+        metric_dict = utils.MetricCalculator.metrics_to_dict(
+            curves.transpose(), type='values'
+        )
+        utils.save_noise_results(
+            metric_dict, noise_scales, checkpoint_model_dir, noise_label
+        )
     calculations.append(curves)
 
 if args.itr is None:

Long-term_Forecasting/utils/metrics.py
@@ -1,91 +1,342 @@
+import os
+import json
+import einops
 import torch
 import torch.nn.functional as F
 import numpy as np
+from typing import NamedTuple
 
 
-def RSE(pred, true):
+class LossResults(NamedTuple):
+    """a docstring"""
+    total_loss : torch.tensor
+    value_value : torch.tensor
+    value_token : torch.tensor
+    token_value : torch.tensor
+    token_token : torch.tensor
+
+class Loss:
+    def __init__(
+            self,
+            value_loss_config,
+            token_loss_config,
+            tokenizer=None,
+            pretrain_embeddings=False
+        ):
+        self.value_config = value_loss_config
+        self.token_config = token_loss_config
+        self.tokenizer = tokenizer
+        self.pretrain_embeddings = pretrain_embeddings
+        
+        self.value_criterion = self.get_criterion(self.value_config)
+        self.token_criterion = self.get_criterion(self.token_config)
+        
+    def __call__(self, pred, truth):
+        value_value = 0
+        value_token = 0
+        token_value = 0
+        token_token = 0
+
+        use_tkn_y_value = self.value_criterion is not None\
+            and self.value_config['token_pred_scale'] is not None
+        use_tkn_y_token = self.token_criterion is not None\
+            and self.token_config['token_pred_scale'] is not None 
+        if self.tokenizer is not None and (use_tkn_y_value or use_tkn_y_token):
+            truth_tokens = self.tokenizer(
+                einops.rearrange(truth, 'b l m -> b m l')
+            )
+            truth_tokens = einops.rearrange(truth_tokens, 'b m l -> b l m')
+        
+        if self.value_criterion is not None:
+            if self.value_config['value_pred_scale'] is not None:
+                value_value = self.value_config['value_pred_scale']\
+                    *self.value_criterion(pred.value_values, truth)
+            #print(self.value_config)
+            #print(self.value_config['token_pred_scale'])
+            if self.value_config['token_pred_scale'] is not None:
+                value_token = self.value_config['token_pred_scale']\
+                    *self.token_criterion(self.tokenizer(pred.value_values, return_logits=True), truth_tokens)
+        if self.token_criterion is not None:
+            if self.token_config['value_pred_scale'] is not None:
+                token_value = self.token_config['value_pred_scale']\
+                    *self.value_criterion(pred.token_values, truth)
+            if self.token_config['token_pred_scale'] is not None:
+                token_token = self.token_config['token_pred_scale']\
+                    *self.token_criterion(pred.token_logits, truth_tokens)
+         
+            """
+            loss += self.token_criterion(
+                pred.token_values,
+                true,
+                pred.token_logits,
+                self.tokenizer(true),
+                'token'
+            )
+            """
+        total_loss = value_value + value_token + token_value + token_token
+        results = LossResults(
+            total_loss,
+            value_value,
+            value_token,
+            token_value,
+            token_token
+        )
+
+        return total_loss, results
+
+    def get_criterion(self, config):
+        if config is None:
+            return None
+        
+        if config['type'] == 'mse':
+            return MSE
+        elif config['type'] == 'mse_cross_entropy':
+            #self.params[f'{prefix}_mse_weight'] = params['mse_weight']
+            #self.params[f'{prefix}_cross_entropy_weight'] = params['cross_entropy_weight']
+            #self.mse = torch.nn.MSELoss
+            return self.MSE_cross_entropy
+        elif config['type'] == 'cross_entropy':
+            return cross_entropy
+        elif config['type'] == 'smape':
+            return SMAPE
+        
+        raise ValueError(f"Cannot handle loss type {config['type']}")
+
+    def MSE_cross_entropy(self, pred, true, pred_logits, true_tokens, prefix):
+        return self.params[f'{prefix}_mse_weight']*MSE(pred, true)\
+            + self.params[f'{prefix}_xentropy_weight']\
+            * F.cross_entropy(pred_logits, true_tokens)
+
+def get_loss(config, tokenizer):
+    if config.pretrain_embeddings:
+        value_loss_config = config.embedding_value_loss_config
+        token_loss_config = config.embedding_token_loss_config
+    else:
+        value_loss_config = config.value_loss_config
+        token_loss_config = config.token_loss_config
+    
+    return Loss(
+        value_loss_config,
+        token_loss_config,
+        tokenizer=tokenizer,
+        pretrain_embeddings=config.pretrain_embeddings
+    )
+
+def RSE(pred, true, *args):
     return torch.sqrt(torch.sum((true - pred) ** 2)) / torch.sqrt(torch.sum((true - true.mean()) ** 2))
 
 
-def CORR(pred, true):
+def CORR(pred, true, *args):
     u = ((true - true.mean(0)) * (pred - pred.mean(0))).sum(0)
     d = torch.sqrt(((true - true.mean(0)) ** 2 * (pred - pred.mean(0)) ** 2).sum(0))
     return (u / d).mean(-1)
 
 
-def MAE(pred, true):
+def MAE(pred, true, *args):
     return torch.mean(torch.abs(pred - true))
 
 
-def MSE(pred, true):
+def MSE(pred, true, *args):
     return torch.mean((pred - true) ** 2)
 
 
-def RMSE(pred, true):
+def RMSE(pred, true, *args):
     return torch.sqrt(MSE(pred, true))
 
 
-def MAPE(pred, true):
+def MAPE(pred, true, *args):
     return torch.mean(torch.abs(100 * (pred - true) / (true +1e-8)))
 
 
-def MSPE(pred, true):
+def MSPE(pred, true, *args):
     return torch.mean(torch.square((pred - true) / (true + 1e-8)))
 
-def SMAPE(pred, true):
+def SMAPE(pred, true, *args):
     return torch.mean(200 * torch.abs(pred - true) / (torch.abs(pred) + torch.abs(true) + 1e-8))
     # return torch.mean(200 * torch.abs(pred - true) / (pred + true + 1e-8))
 
-def ND(pred, true):
+def ND(pred, true, *args):
     return torch.mean(torch.abs(true - pred)) / torch.mean(torch.abs(true))
 
-def metric(pred, true):
-    mae = MAE(pred, true)
-    mse = MSE(pred, true)
-    rmse = RMSE(pred, true)
-    mape = MAPE(pred, true)
-    mspe = MSPE(pred, true)
-    smape = SMAPE(pred, true)
-    nd = ND(pred, true)
 
-    return torch.tensor([mae, mse, rmse, mape, mspe, smape, nd])
+value_labels = ["mae", "mse", "rmse", "mape", "mspe", "smape", "nd"]
+token_labels = ["cross_entropy", "accuracy"]
+class MetricCalculator:
+    def __init__(self, tokenizer=None):
+        self.tokenizer = tokenizer
+        self.n_values = len(value_labels)
+        self.n_tokens = len(token_labels)
+    
+    def __call__(self, pred, truth):
+        if self.tokenizer is not None:
+            truth_tokens = self.tokenizer(
+                einops.rearrange(truth, 'b l m -> b m l')
+            )
+            truth_tokens = einops.rearrange(truth_tokens, 'b m l -> b l m')
+            truth_tokens = truth_tokens.to(truth.device)
 
+        if pred.value_values is not None:
+            value_values = MetricCalculator.value_metrics(
+                pred.value_values, truth
+            )
+            if self.tokenizer is not None:
+                logits = self.tokenizer(
+                    einops.rearrange(pred.value_values, 'b l m -> b m l'),
+                    return_logits=True
+                ).to(truth.device)
+                logits = einops.rearrange(logits, 'b m l c -> b l m c')
+                value_tokens = MetricCalculator.token_metrics(logits, truth_tokens)
+            else:
+                value_tokens = torch.ones(self.n_tokens)*torch.nan 
+        else:
+            value_values = torch.ones(self.n_values)*torch.nan
+            value_tokens = torch.ones(self.n_tokens)*torch.nan 
+        
+        if pred.token_values is not None:
+            token_values = MetricCalculator.value_metric(
+                pred.token_values, truth
+            )
+        else:
+            token_values = torch.ones(self.n_values)*torch.nan
+        
+        if pred.token_logits is not None:
+            token_tokens = MetricCalculator.token_metrics(
+                pred.token_logits, truth_tokens
+            )
+        else:
+            token_tokens = torch.ones(self.n_tokens)*torch.nan
+
+        value_results = torch.stack((value_values, token_values))
+        token_results = torch.stack((value_tokens, token_tokens))
+        return value_results, token_results
+
+    @staticmethod
+    def value_metrics(pred, truth, *args):
+        mae = MAE(pred, truth)
+        mse = MSE(pred, truth)
+        rmse = RMSE(pred, truth)
+        mape = MAPE(pred, truth)
+        mspe = MSPE(pred, truth)
+        smape = SMAPE(pred, truth)
+        nd = ND(pred, truth)
+
+        return torch.tensor([mae, mse, rmse, mape, mspe, smape, nd])
+    
+    @staticmethod
+    def token_metrics(pred, truth, *args):
+        xentropy = cross_entropy(pred, truth)
+        accr = accuracy(pred, truth)
+
+        return torch.tensor([xentropy, accr])
+
+    @staticmethod
+    def loss_metrics_to_dict(
+        losses,
+        value_metrics,
+        token_metrics,
+        losses_std=None,
+        value_std=None,
+        token_std=None,
+    ):
+        output_value = MetricCalculator.metrics_to_dict(
+            value_metrics, value_std, type='value'
+        )
+        output_token = MetricCalculator.metrics_to_dict(
+            token_metrics, token_std, type='token'
+        )
+        output = {**output_value, **output_token}
+
+        loss_labels = [
+            'loss',
+            'value_value_loss',
+            'value_token_loss',
+            'token_value_loss',
+            'token_token_loss'
+        ]
+        if losses_std is None:
+            losses_std = np.ones_like(losses)*np.nan
+        for i, (val, std) in enumerate(zip(losses, losses_std)):
+            output[loss_labels[i]] = val
+            output[loss_labels[i] + '_std'] = std
+
+        return output
+
+    @staticmethod
+    def metrics_to_dict(metrics, metric_std=None, type='values', prefix=''):
+        if metric_std is None:
+            metric_std = np.ones_like(metrics)*np.nan
+        
+        if len(metrics.shape) == 2:
+            if type[-1] == 's':
+                type = type[:-1]
+            output = {}
+            for i, (mtrs, mtrs_std) in enumerate(zip(metrics, metric_std)):
+                prefix = 'value' if i == 0 else 'token'
+                prefix += '_' + type + "_"
+                output = {**output, **MetricCalculator.metrics_to_dict(
+                    mtrs, mtrs_std, type=type, prefix=prefix
+                )}
+            return output
+
+        if type == 'values' or type == 'value':
+            labels = value_labels
+        elif type == 'tokens' or type == 'token':
+            labels = token_labels
+        else:
+            raise ValueError(f"Cannot handle type '{type}'")
+        
+        if len(labels) == len(metrics)-1:
+            labels = ['loss'] + labels
+        if len(labels) == len(metrics):
+            output = {prefix + labels[i] : metrics[i] for i in range(len(labels))}
+            for i in range(len(labels)):
+                output[prefix + labels[i]+"_std"] = metric_std[i]
+        else:
+            raise ValueError("Input metrics are not the right length")
+
+        return output
+
+"""
 def get_labels():
     return [
         "mae", "mse", "rmse", "mape", "mspe", "smape", "nd"
     ]
+"""
 
-def metrics_to_dict(metrics, metric_std=None):
-    labels = get_labels()
-    if len(labels) == len(metrics):
-        output = {labels[i] : metrics[i] for i in range(len(labels))}
-        if metric_std is not None:
-            for i in range(len(labels)):
-                output[labels[i]+"_std"] = metric_std[i]
-    elif len(labels) == len(metrics)-1: 
-        output = {labels[i] : metrics[i+1] for i in range(len(labels))}
-        output["loss"] = metrics[0]
-        if metric_std is not None:
-            for i in range(len(labels)):
-                output[labels[i]+"_std"] = metric_std[i+1]
-            output["loss_std"] = metric_std[0]
+def cross_entropy(logits, tokens_y):
+    return F.cross_entropy(
+        einops.rearrange(logits, 'b l m c -> b c l m'), tokens_y
+    )
+
+def accuracy(input, tokens_y):
+    # If input is logits convert to tokens
+    if len(input.shape) > len(tokens_y.shape):
+        input = torch.argmax(input, -1)
+    return torch.mean((input == tokens_y).to(torch.float32))
+
+def load_noise_metrics(results_dir, label, noise, require_loss=True):
+    filename = os.path.join(results_dir, f"noise_{label}_results.json")
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            noise_dict = json.load(file)
     else:
-        raise ValueError("Input metrics are not the right length")
+        return None
+    
+    labels = get_labels()
+    if require_loss:
+        labels.insert(0, 'loss')
+    
+    if noise not in noise_dict['std_scales']:
+        return None
+    idx = noise_dict['std_scales'].index(noise)
+    results = []
+    for lbl in labels:
+        if lbl not in noise_dict.keys():
+            return None
+        results.append(noise_dict[lbl][idx])
+    
+    return results
 
-    return output
-
-def cross_entropy(logits, data_y=None, tokens_y=None):
-    return F.cross_entropy(logits.reshape(-1, logits.shape[-1]), tokens_y.view(-1))
-
-
-class Loss:
-    def __init__(self, names, weights):
-        self.names = names
-        self.weights = weights
-
-        self.loss_dict = {
-            "mse" : MSE
-        }
 
 """
 def MAE_tokens(logits, data_y, tokenizer, tokens_y=None):